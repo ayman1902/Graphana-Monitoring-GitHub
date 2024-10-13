ef1340b075df5ce8ab79695ca0d78c6ce38a7c15
ayman1902 <aymanebelhaje@example.com>
2024-10-13 18:44:55 +0100

    f

---
+++ Terraform/main.tf
@@ -0,0 +1,89 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "eu-west-1"  # London region
+  access_key = var.aws_access_key  # Use environment variable for access key
+  secret_key = var.aws_secret_key  # Use environment variable for secret key
+}
+
+# To Generate Private Key
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  default     = "my-ssh-key"  # You can change the key pair name here if needed
+}
+
+# Create Key Pair for Connecting EC2 via SSH
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+# Save PEM file locally
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = "${var.key_name}.pem"  # Save the key as a PEM file
+}
+
+resource "aws_security_group" "sg_ec2" {
+  name        = "sg_ec2"
+  description = "Security group for EC2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow inbound traffic on port 3000 for Grafana
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # This allows access from anywhere. You can restrict to your IP range if needed.
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# Create an EC2 instance
+resource "aws_instance" "public_instance" {
+  ami                    = "ami-0a422d70f727fe93e"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+
+  tags = {
+    Name = "public_instance"
+  }
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+  }
+}
+output "instance_public_ip" {
+  value = aws_instance.public_instance.public_ip
+}
+
+# Variables for AWS access keys
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
